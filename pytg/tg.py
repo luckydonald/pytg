0cd21b65a6b2dcbcfb5e28ca52fd767f26c6af6b
E A Faisal <eafaisal@gmail.com>
2014-01-21 11:11:14 +0800

    Added handler for selected service messages and media

--- pytg/tg.py
+++ pytg/tg.py
@@ -12,7 +12,7 @@ def dialog_list(target):
         while True:
             line = (yield)
             if '{print_message}' in line or '{end_print_message}' in line or \
-               '{user_status}' in line:
+               '{user_status}' in line or '{service_message}' in line:
                 continue
             m = unread_user.search(clear_prompt(remove_color(line)).strip())
             if m:
@@ -47,7 +47,7 @@ def chat_info(target):
         while True:
             line = (yield)
             if '{print_message}' in line or '{end_print_message}' in line or \
-               '{user_status}' in line:
+               '{user_status}' in line or '{service_message}' in line:
                 continue
             m = chat_info_header.search(clear_prompt(remove_color(line)).strip())
             if m:
@@ -95,7 +95,8 @@ def user_info(target):
         while True:
             line = (yield)
             if '{print_message}' in line or '{end_print_message}' in line or \
-               '{user_status}' in line or 'unread' in line:
+               '{user_status}' in line or '{service_message}' in line or \
+               'unread' in line:
                 continue
             m = user_info_header.search(clear_prompt(remove_color(line)).strip())
             if m:
@@ -115,54 +116,6 @@ def user_info(target):
                 arg['phone'] = m.group('phone')
                 target.send(arg)
                 header_found = False
-    except GeneratorExit:
-        pass
-
-@coroutine
-def message(target):
-    if type(target) is not GeneratorType:
-        raise TypeError('target must be GeneratorType')
-    try:
-        while True:
-            line = (yield)
-            if '{print_message}' not in line or '{end_print_message}' not in line:
-                continue
-            m = print_message.search(clear_prompt(remove_color(line)).strip())
-            if m:
-                arg = {
-                    'type': 'message', 'msgid': m.group('msgid'),
-                    'timestamp': m.group('timestamp'),
-                    'message': m.group('message')
-                }
-                tmpchat, tmpuser = m.group('chat'), m.group('user')
-                arg['peer'] = 'group' if tmpchat else 'user'
-                if tmpchat:
-                    if '#' in tmpchat:
-                        arg['group'], arg['gid'] = tmpchat.split('#')
-                        arg['cmdgroup'] = arg['group'].replace(' ', '_')
-                    else:
-                        arg['group'], arg['cmdgroup'], arg['gid'] = None, None, tmpchat
-                if '#' in tmpuser:
-                    arg['user'], arg['uid'] = tmpuser.split('#')
-                    arg['cmduser'] = arg['user'].replace(' ', '_')
-                else:
-                    arg['user'], arg['cmduser'], arg['uid'] = None, None, tmpuser
-                if arg['peer'] == 'user':
-                    arg['ownmsg'] = True if m.group('dir') == '«««' else False
-                if m.group('media'):
-                    if 'photo' in m.group('media'):
-                        pass
-                    elif 'video' in m.group('media'):
-                        pass
-                    elif 'audio' in m.group('media'):
-                        pass
-                    elif 'document' in m.group('media'):
-                        pass
-                    elif 'geo' in m.group('media'):
-                        pass
-                    elif 'contact' in m.group('media'):
-                        pass
-                target.send(arg)
     except GeneratorExit:
         pass
 
@@ -193,7 +146,8 @@ def contact_list(target):
         while True:
             line = (yield)
             if '{print_message}' in line or '{end_print_message}' in line or \
-               '{user_status}' in line or 'unread' in line:
+               '{user_status}' in line or '{service_message}' in line or \
+               'unread' in line:
                 continue
             m = contact_list_data.search(clear_prompt(remove_color(line)).strip())
             if m:
@@ -202,6 +156,102 @@ def contact_list(target):
                     'user': m.group('user'), 'cmduser': m.group('cmduser'),
                     'phone': m.group('phone')
                 }
+                target.send(arg)
+    except GeneratorExit:
+        pass
+
+@coroutine
+def message(target):
+    if type(target) is not GeneratorType:
+        raise TypeError('target must be GeneratorType')
+    try:
+        while True:
+            line = (yield)
+            if '{print_message}' not in line or '{end_print_message}' not in line:
+                continue
+            m = print_message_data.search(clear_prompt(remove_color(line)).strip())
+            if m:
+                arg = {
+                    'type': 'message', 'msgid': m.group('msgid'),
+                    'timestamp': m.group('timestamp'),
+                    'message': m.group('message'), 'media': None
+                }
+                tmpchat, tmpuser = m.group('chat'), m.group('user')
+                arg['peer'] = 'group' if tmpchat else 'user'
+                if tmpchat:
+                    if '#' in tmpchat:
+                        arg['group'], arg['gid'] = tmpchat.split('#')
+                        arg['cmdgroup'] = arg['group'].replace(' ', '_')
+                    else:
+                        arg['group'], arg['cmdgroup'], arg['gid'] = None, None, tmpchat
+                if '#' in tmpuser:
+                    arg['user'], arg['uid'] = tmpuser.split('#')
+                    arg['cmduser'] = arg['user'].replace(' ', '_')
+                else:
+                    arg['user'], arg['cmduser'], arg['uid'] = None, None, tmpuser
+                if arg['peer'] == 'user':
+                    arg['ownmsg'] = True if m.group('dir') == '«««' else False
+                if m.group('media'):
+                    if 'photo' in m.group('media'):
+                        tmp = m.group('media').split(' ', 1)
+                        arg['media'] = {'type': tmp[0], 'caption': None}
+                        if len(tmp) == 2:
+                            arg['media'] = {'caption': tmp[1]}
+                    elif 'video' in m.group('media') or \
+                        'audio' in m.group('media') or \
+                        'contact' in m.group('media'):
+                        arg['media'] = {'type': m.group('media')}
+                    elif 'document' in m.group('media'):
+                        arg['media'] = {'type': 'document', 'caption': None, 'mime': None}
+                        tmp = m.group('media').split(':')
+                        try:
+                            arg['media']['caption'] = tmp[0].split(' ', 1)[1]
+                        except IndexError:
+                            pass
+                        try:
+                            arg['media']['mime'] = tmp[1].strip().split(' ', 1)[1]
+                        except IndexError:
+                            pass
+                    elif 'geo' in m.group('media'):
+                        arg['media'] = {'type': 'geo', 'link': m.group('geolink')}
+                target.send(arg)
+    except GeneratorExit:
+        pass
+
+@coroutine
+def service_message(target):
+    if type(target) is not GeneratorType:
+        raise TypeError('target must be GeneratorType')
+    try:
+        while True:
+            line = (yield)
+            if '{service_message}' not in line or '{service_message}' not in line:
+                continue
+            m = service_message_data.search(clear_prompt(remove_color(line)).strip())
+            if m:
+                arg = {
+                    'type': 'service_message', 'msgid': m.group('msgid'),
+                    'timestamp': m.group('timestamp'),
+                    'message': m.group('message'), 'action': m.group('action'),
+                }
+                if m.group('gid'):
+                    arg['group'], arg['gid'] = m.group('group'), arg('gid')
+                    arg['cmdgroup'] = arg['group'].replace(' ', '_')
+                else:
+                    arg['group'], arg['cmdgroup'], arg['gid'] = None, None, m.group('group')
+                if m.group('uid'):
+                    arg['user'], arg['uid'] = m.group('user'), arg('uid')
+                    arg['cmduser'] = arg['user'].replace(' ', '_')
+                else:
+                    arg['user'], arg['cmduser'], arg['uid'] = None, None, m.group('user')
+                if arg['action'] == 'changed title to':
+                    arg['actionarg'] = m.group('arg')
+                else:
+                    tmp = m.group('arg').split('#')
+                    if len(tmp) == 2:
+                        arg['actionarg'] = {'user': None, 'uid': tmp[1]}
+                    else:
+                        arg['actionarg'] = {'user': tmp[1], 'uid': tmp[2]}
                 target.send(arg)
     except GeneratorExit:
         pass
