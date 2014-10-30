fa22bf13a0b0f6a82fe9e43690467c550b0b51b1
luckydonald <luckydonald@flutterb.at>
2014-10-30 01:31:03 +0100

    improoved python 3 compatibility.

--- pytg/__init__.py
+++ pytg/__init__.py
@@ -14,7 +14,7 @@ class TelegramError(Exception):
 class Telegram(object):
 	_proc, tgin = None, None
 	_pipeline, _callables = None, []
-	_buffer, _banner_found = '', False
+	_buffer, _banner_found = b'', False
 	_ignore = ['> \n','>\n']
 	ready = False
 	def __init__(self, telegram, pubkey_file):
@@ -44,10 +44,12 @@ class Telegram(object):
 			raise TelegramError('telegram not running')
 
 		try:
-			c = self._proc.stdout.read(1)
+			c = bytes(self._proc.stdout.read(1), 'utf-8')
 		except:
-			c = ''
-		self._buffer += c
+			c = b''
+		if not c:
+			c = b''
+		self._buffer = self._buffer + c
 		if not self._banner_found and c == '\n' and 'conditions' in self._buffer:
 			self._banner_found = True
 			self._buffer = ''
@@ -63,13 +65,13 @@ class Telegram(object):
 				self._buffer = ''
 
 	def quit(self):
-		self._proc.communicate('quit\n')
+		self._proc.communicate(b'quit\n')
 		if len(self._buffer) > 0:
 			self._pipeline.send(self._buffer)
 		self._pipeline.close()
 
 	def safe_quit(self):
-		self._proc.communicate('safe_quit\n')
+		self._proc.communicate(b'safe_quit\n')
 		if len(self._buffer) > 0:
 			self._pipeline.send(self._buffer)
 		self._pipeline.close()
@@ -83,66 +85,66 @@ class Telegram(object):
 
 
 	def msg(self, peer, message):
-		self.tgin.write(' '.join(['msg', peer, message]) + '\n')
+		self.tgin.write(bytes(' '.join(['msg', peer, message]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_audio(self, peer, path):
-		self.tgin.write(' '.join(['send_audio', peer, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_audio', peer, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_contact(self, peer, phone, first_name, last_name):
-		self.tgin.write(' '.join(['send_contact', peer, phone, first_name, last_name]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_contact', peer, phone, first_name, last_name]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_document(self, peer, path):
-		self.tgin.write(' '.join(['send_document', peer, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_document', peer, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_photo(self, peer, path):
-		self.tgin.write(' '.join(['send_photo', peer, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_photo', peer, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_video(self, peer, path):
-		self.tgin.write(' '.join(['send_video', peer, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_video', peer, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_location(self, peer, latitude, longitude):
-		self.tgin.write(' '.join(['send_location', peer, latitude, longitude]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_location', peer, latitude, longitude]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def send_text(self, peer, path):
-		self.tgin.write(' '.join(['send_text', peer, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['send_text', peer, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def status_online(self):
-		self.tgin.write('status_online\n')
+		self.tgin.write(b'status_online\n')
 		self.tgin.flush()
 
 	def status_offline(self):
-		self.tgin.write('status_offline\n')
+		self.tgin.write(b'status_offline\n')
 		self.tgin.flush()
 
 
 	def dialog_list(self):
-		self.tgin.write('dialog_list\n')
+		self.tgin.write(b'dialog_list\n')
 		self.tgin.flush()
 
 	def chat_info(self, chat):
-		self.tgin.write(''.join(['chat_info ', chat, '\n']))
+		self.tgin.write(bytes(''.join(['chat_info ', chat, '\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def chat_add_user(self, chat, user, msgs_to_forward="100"):
 		"""
 		Adds user to chat. Sends him last msgs_to_forward message from this chat. Default "100"
 		"""
-		self.tgin.write(' '.join(['chat_add_user', chat, user,msgs_to_forward]) + '\n')
+		self.tgin.write(bytes(' '.join(['chat_add_user', chat, user,msgs_to_forward]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def chat_del_user(self, chat, user):
 		"""
 		Deletes user from chat
 		"""
-		self.tgin.write(' '.join(['chat_del_user', chat, user]) + '\n')
+		self.tgin.write(bytes(' '.join(['chat_del_user', chat, user]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 # create_group_chat <user> <chat topic> - creates a groupchat with user, use chat_add_user to add more users
@@ -153,39 +155,39 @@ class Telegram(object):
 		:param chat_topic: name of the chat.  #TODO: Can this contain spaces, does it need quotes?
 		"""
 
-		self.tgin.write(' '.join(['create_group_chat', user, chat_topic]) + '\n') #TODO: Can chat_topic contain spaces, does it need quotes?
+		self.tgin.write(bytes(' '.join(['create_group_chat', user, chat_topic]) + '\n', 'utf-8')) #TODO: Can chat_topic contain spaces, does it need quotes?
 		self.tgin.flush()
 
 	def user_info(self, user):
-		self.tgin.write(''.join(['user_info ', user, '\n']))
+		self.tgin.write(bytes(''.join(['user_info ', user, '\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def mark_read(self, peer):
-		self.tgin.write(''.join(['mark_read ', peer, '\n']))
+		self.tgin.write(bytes(''.join(['mark_read ', peer, '\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def chat_with_peer(self, peer):
-		self.tgin.write(''.join(['chat_with_peer ', peer, '\n']))
+		self.tgin.write(bytes(''.join(['chat_with_peer ', peer, '\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def contact_list(self):
-		self.tgin.write('contact_list\n')
+		self.tgin.write(b'contact_list\n')
 		self.tgin.flush()
 
 	def del_contact(self, user):
-		self.tgin.write(''.join(['del_contact ', user, '\n']))
+		self.tgin.write(bytes(''.join(['del_contact ', user, '\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def fwd(self, peer, msg_id):
-		self.tgin.write(' '.join(['fwd', peer, msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['fwd', peer, msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def fwd_media(self, peer, msg_id):
-		self.tgin.write(' '.join(['fwd_media', peer, msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['fwd_media', peer, msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	def raw_input(self, raw):
-		self.tgin.write(''.join([raw,'\n']))
+		self.tgin.write(bytes(''.join([raw,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 
@@ -193,21 +195,21 @@ class Telegram(object):
 		"""
 		Sets chat photo. Photo will be cropped to square
 		"""
-		self.tgin.write(' '.join(['chat_set_photo', chat, path]) + '\n')
+		self.tgin.write(bytes(' '.join(['chat_set_photo', chat, path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def set_profile_photo(self, path):
 		"""
 		Sets profile photo. Photo will be cropped to square
 		"""
-		self.tgin.write(' '.join(['set_profile_photo', path]) + '\n')
+		self.tgin.write(bytes(' '.join(['set_profile_photo', path]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def export_card(self):
 		"""
 		Prints card that can be imported by another user with import_card method
 		"""
-		self.tgin.write('export_card\n')
+		self.tgin.write(b'export_card\n')
 		self.tgin.flush()
 
 	def import_card(self,card):
@@ -215,14 +217,14 @@ class Telegram(object):
 		Gets user by card and prints it name. You can then send messages to him as usual
 		:param card: Is a String like 0301adca:42eb522a:2f02aca9:4562e9f1:586a195b
 		"""
-		self.tgin.write(' '.join(['import_card', card]) + '\n')
+		self.tgin.write(bytes(' '.join(['import_card', card]) + '\n', 'utf-8'))
 		self.tgin.flush()
 
 	def main_session(self):
 		"""
 		Sends updates to this connection (or terminal). Useful only with listening socket
 		"""
-		self.tgin.write('main_session\n')
+		self.tgin.write(b'main_session\n')
 		self.tgin.flush()
 
 	# load_photo <msg-id>     Downloads file to downloads dirs. Prints file name after download end
@@ -230,7 +232,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_photo', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_photo', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	# load_video <msg-id>     Downloads file to downloads dirs. Prints file name after download end
@@ -238,7 +240,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_video', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_video', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 
@@ -247,7 +249,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_audio', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_audio', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	# load_document <msg-id>  Downloads file to downloads dirs. Prints file name after download end
@@ -255,7 +257,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_document', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_document', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	# load_video_thumb <msg-id>       Downloads file to downloads dirs. Prints file name after download end
@@ -263,7 +265,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_video_thumb', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_video_thumb', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 	# load_document_thumb <msg-id>    Downloads file to downloads dirs. Prints file name after download end
@@ -271,7 +273,7 @@ class Telegram(object):
 		"""
 		Downloads file to downloads dirs.
 		"""
-		self.tgin.write(' '.join(['load_document_thumb', msg_id,'\n']))
+		self.tgin.write(bytes(' '.join(['load_document_thumb', msg_id,'\n']), 'utf-8'))
 		self.tgin.flush()
 
 
