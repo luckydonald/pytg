30cbbfadb79d1b294f6a2fd0a639f4a5cd83a8ff
luckydonald <luckydonald@flutterb.at>
2015-02-02 18:02:29 +0100

    Working Dump example added.

--- examples/dump.py
+++ examples/dump.py
@@ -3,12 +3,19 @@ from pytg2 import Telegram
 from pytg2.utils import coroutine
 
 @coroutine
-def test(tg):
-	while True:
-		msg = (yield)
-		print('Message "{text}", full dump: {array}'.format(text=str( msg['text'] ), array=str( msg )))
+def example_function(tg):
+	try:
+		while True:
+			msg = (yield)
+			print('Message "{text}", full dump: {array}'.format(text=str( msg['text'] ), array=str( msg )))
+			if msg['out'] == False:
+				tg._do_send("msg chat#3921114 Ping")
+	except KeyboardInterrupt:
+		print("Exiting")
 
 if __name__ == '__main__':
-	tg = Telegram() #get a Telegram Connector instance
+	tg = Telegram(port_out=9034) #get a Telegram Connector instance
 	tg.start() #start the Connector.
-	tg.message(test(tg)) # add "test" function as listeners. You can supply arguments here (like tg).
+	tg.message(example_function(tg)) # add "example_function" function as listeners. You can supply arguments here (like tg).
+	# continues here, after exiting while loop in example_function()
+	tg.stop()
