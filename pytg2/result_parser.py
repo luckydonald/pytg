69b1ad81165ecaba6e6b831736db4fdee0df87ae
luckydonald <luckydonald@flutterb.at>
2015-03-14 11:14:21 +0100

    added nothing and something parser

--- pytg2/result_parser.py
+++ pytg2/result_parser.py
@@ -1,12 +1,26 @@
 __author__ = 'luckydonald'
 from .encoding import to_unicode as u
+from .encoding import to_native as n
+
 
 class IllegalResponseException(Exception):
 	pass
+
+
+def nothing(value):
+	if value and len(value) > 1:
+		raise IllegalResponseException("Should return nothing.")
+	return True
+
+
+def something(value):
+	if not (value and len(value) > 1):
+		raise IllegalResponseException("Should return something.")
+
 
 def success_fail(value):
 	if value == u("SUCCESS"):
 		return True
 	if value == u("FAIL"):
 		return False
-	raise IllegalResponseException(value)
+	raise IllegalResponseException("Found \"%s\"" % n(value))
