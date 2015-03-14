ca6edf59741818669f9bc96017c387a62dab9b39
luckydonald <luckydonald@flutterb.at>
2015-03-14 11:13:33 +0100

    added UnknownFunction (Exception) renamed `unicode_string` > `unescaped_unicode_string`

--- pytg2/argumenttypes.py
+++ pytg2/argumenttypes.py
@@ -1,5 +1,10 @@
 __author__ = 'luckydonald'
 from . import encoding
+from os import path # file checking.
+
+class UnknownFunction(Exception):
+	pass
+
 
 def none(value):
 	if value == None:
@@ -7,7 +12,7 @@ def none(value):
 	return False
 
 def peer(value):
-	if not unicode_string(value):
+	if not unescaped_unicode_string(value):
 		return False
 	if " " in value:
 		return False
@@ -25,7 +30,7 @@ def secret_chat(value):
 	return peer(value)
 
 def unicode_string(value):
-	return isinstance(value, encoding.text_type)
+	return isinstance(value, encoding.text_type) #TODO
 
 def unescaped_unicode_string(value):
 	"""
@@ -45,6 +50,10 @@ def positive_number(value):
 			return True
 	return False
 
+def file(value):
+	print("Got file: [{}]".format(value)) #TODO remove
+	return path.isfile(encoding.native_type(value))
+
 def nonnegative_number(value):
 	if number(value):
 		if value >= 0:
@@ -58,7 +67,26 @@ def msg_id(value):
 			return positive_number(int_val)
 		except:
 			return False
-	return positive_number()
+	return positive_number(value)
 
 def optional():
 	return True
+
+from .utils import escape
+def validate_input(function_name, arguments, arguments_types):
+		if (len(arguments) != len(arguments_types)):
+			raise ValueError("Error in function {function_name}: {expected_number} paramters expected, but {given_number} were given.".format(function_name=function_name, expected_number=len(arguments_types), given_number=len(args)))
+		i = 0
+		new_args = []
+		for arg in arguments:
+			func_type = arguments_types[i]
+			# arg is the given one, which should be func_type.
+			if not func_type(arg):
+				raise ValueError("Error in function {function_name}: parameter {number} is not type {type}.".format(function_name=function_name, number=i, type=func_type.__name__))
+			if func_type == unicode_string:
+				new_args.append(encoding.to_unicode(escape(arg)))
+			else:
+				new_args.append(encoding.to_unicode(str(arg)))
+			i += 1
+		# end for
+		return new_args
