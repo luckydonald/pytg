63f2520b8993de41c1f5a6c1fac87eb20c57ff53
luckydonald <luckydonald@flutterb.at>
2015-03-14 11:16:10 +0100

    replaced some `args.unicode_string` with `args.file` arguments. Added result parsers. Added timeouts.

--- pytg2/sender.py
+++ pytg2/sender.py
@@ -1,7 +1,9 @@
+from pytg2.argumenttypes import UnknownFunction
+
 __author__ = 'luckydonald'
 from . import argumenttypes as args
 from . import result_parser as res
-from .utils import suppress_context, escape
+from .utils import escape
 from .encoding import to_native as n
 from .encoding import to_unicode as u
 from .encoding import to_binary as b
@@ -16,65 +18,72 @@ SOCKET_SIZE = 1 << 25
 FUNC_CMD  = 0
 FUNC_ARGS = 1
 FUNC_RES  = 2
+FUNC_TIME = 3
 __all__ = ["FUNC_CMD", "FUNC_ARGS", "FUNC_RES", "functions", "Sender"]
 functions = {
-	"get_contact_list":		["contact_list",		[],																],
-	"get_dialog_list": 		["dialog_list", 		[],																],
-	"rename_chat": 			["rename_chat", 		[args.chat, args.unicode_string],								],
-	"send_msg": 			["msg", 				[args.peer, args.unicode_string],								res.success_fail],
-	"send_typing": 			["send_typing", 		[args.peer, args.nonnegative_number],							],
-	"send_typing_abort": 	["send_typing_abort", 	[args.peer],													],
-	"send_photo": 			["send_photo", 			[args.peer, args.unicode_string],								],
-	"send_video": 			["send_video", 			[args.peer, args.unicode_string],								],
-	"send_audio": 			["send_audio", 			[args.peer, args.unicode_string],								],
-	"send_document": 		["send_document", 		[args.peer, args.unicode_string],								],
-	"send_file": 			["send_file", 			[args.peer, args.unicode_string],								],
-	"send_text": 			["send_text", 			[args.peer, args.unicode_string]								],
-	"send_location": 		["send_location", 		[args.peer, args.double, args.double],							],
-	"load_photo": 			["load_photo", 			[args.msg_id],													],
-	"load_video": 			["load_video", 			[args.msg_id],													],
-	"load_video_thumb": 	["load_video_thumb", 	[args.msg_id],													],
-	"load_audio": 			["load_audio", 			[args.msg_id],													],
-	"load_document": 		["load_document", 		[args.msg_id],													],
-	"load_document_thumb": 	["load_document_thumb", [args.msg_id],													],
-	"fwd_msg": 				["fwd",		 			[args.peer, args.msg_id],										],
-	"fwd_media": 			["fwd_media", 			[args.peer, args.msg_id],										],
-	"chat_info": 			["chat_info", 			[args.chat],													],
-	"chat_set_photo": 		["chat_set_photo", 		[args.chat, args.unicode_string],								],
-	"chat_add_user": 		["chat_add_user", 		[args.chat, args.user],											],
-	"chat_del_user": 		["chat_del_user", 		[args.chat, args.user],											],
-	"create_secret_chat": 	["create_secret_chat", 	[args.user],													],
-	"create_group_chat": 	["create_group_chat", 	[args.user, args.unicode_string],								],
-	"user_info": 			["user_info", 			[args.user],													],
-	"get_history": 			["history", 			[args.peer, args.nonnegative_number],							],
-	"add_contact": 			["add_contact", 		[args.unicode_string, args.unicode_string, args.unicode_string],],
-	"del_contact": 			["del_contact", 		[args.user],													],
-	"rename_contact": 		["rename_contact", 		[args.unicode_string, args.unicode_string, args.unicode_string],],
-	"msg_search": 			["search", 				[args.peer, args.unicode_string],								],
-	"msg_global_search": 	["search", 				[args.unicode_string],											],
-	"mark_read": 			["mark_read", 			[args.peer],													],
-	"set_profile_photo": 	["set_profile_photo", 	[args.unicode_string],											],
-	"set_profile_name": 	["set_profile_name", 	[args.unicode_string],											],
-	"delete_msg": 			["delete_msg", 			[args.msg_id],													],
-	"restore_msg": 			["restore_msg", 		[args.positive_number],											],
-	"accept_secret_chat": 	["accept_secret_chat", 	[args.secret_chat],												],
-	"send_contact": 		["send_contact", 		[args.peer, args.unicode_string, args.unicode_string, args.unicode_string],],
-	"status_online": 		["status_online", 		[],																],
-	"status_offline": 		["status_offline", 		[],																],
-	"quit": 				["quit", 				[],																],
-	"safe_quit": 			["safe_quit",	 		[],																],
-	"raw": 					["", 					[args.unescaped_unicode_string],								]
+	# function to call      # actual telegram command  # required arguments  # expected return type (parser)  # timeout (None = global default)
+	"get_contact_list":		["contact_list",		[],																res.something, 	None],
+	"get_dialog_list": 		["dialog_list", 		[],																res.something, 	None],
+	"rename_chat": 			["rename_chat", 		[args.chat, args.unicode_string],								res.success_fail, None],
+	"send_msg": 			["msg", 				[args.peer, args.unicode_string],								res.success_fail, None],
+	"send_typing": 			["send_typing", 		[args.peer, args.nonnegative_number],							res.success_fail, None],
+	"send_typing_abort": 	["send_typing_abort", 	[args.peer],													res.success_fail, None],
+	"send_photo": 			["send_photo", 			[args.peer, args.file],								res.success_fail, 60.0],
+	"send_video": 			["send_video", 			[args.peer, args.file],								res.success_fail, 60.0],
+	"send_audio": 			["send_audio", 			[args.peer, args.file],								res.success_fail, 60.0],
+	"send_document": 		["send_document", 		[args.peer, args.file],								res.success_fail, 60.0],
+	"send_file": 			["send_file", 			[args.peer, args.file],								res.success_fail, 60.0],
+	"send_text": 			["send_text", 			[args.peer, args.file],								res.success_fail, 60.0],
+	"send_location": 		["send_location", 		[args.peer, args.double, args.double],							res.success_fail, None],
+	"load_photo": 			["load_photo", 			[args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"load_video": 			["load_video", 			[args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"load_video_thumb": 	["load_video_thumb", 	[args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"load_audio": 			["load_audio", 			[args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"load_document": 		["load_document", 		[args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"load_document_thumb": 	["load_document_thumb", [args.msg_id],													res.something, 	60.0], #String saying something and a filepath
+	"fwd_msg": 				["fwd",		 			[args.peer, args.msg_id],										res.success_fail, None],
+	"fwd_media": 			["fwd_media", 			[args.peer, args.msg_id],										res.success_fail, None],
+	"chat_info": 			["chat_info", 			[args.chat],													res.something, None],
+	"chat_set_photo": 		["chat_set_photo", 		[args.chat, args.unicode_string],								res.success_fail, None],
+	"chat_add_user": 		["chat_add_user", 		[args.chat, args.user],											res.something, 	60.0],
+	"chat_del_user": 		["chat_del_user", 		[args.chat, args.user],											res.success_fail, None],
+	"create_secret_chat": 	["create_secret_chat", 	[args.user],													res.success_fail, None],
+	"create_group_chat": 	["create_group_chat", 	[args.unicode_string, args.user],								res.success_fail, None],
+	"user_info": 			["user_info", 			[args.user],													res.something, None],
+	"get_history": 			["history", 			[args.peer, args.nonnegative_number],							res.something, None],
+	"add_contact": 			["add_contact", 		[args.unicode_string, args.unicode_string, args.unicode_string],res.something, None], #returns the new name
+	"rename_contact": 		["rename_contact", 		[args.user, args.unicode_string, args.unicode_string],			res.something, None], #returns the new name
+	"del_contact": 			["del_contact", 		[args.user],													res.success_fail, None],
+	"msg_search": 			["search", 				[args.peer, args.unicode_string],								res.something, None], #ret: formated messages
+	"msg_global_search": 	["search", 				[args.unicode_string],											res.something, None], #ret: formated messages
+	"mark_read": 			["mark_read", 			[args.peer],													res.success_fail, None],
+	"set_profile_photo": 	["set_profile_photo", 	[args.file],											res.something, 	60.0], #TODO
+	"set_profile_name": 	["set_profile_name", 	[args.unicode_string, args.unicode_string],						res.something, 	60.0], #ret: new name
+	"delete_msg": 			["delete_msg", 			[args.msg_id],													res.success_fail, None],
+	"restore_msg": 			["restore_msg", 		[args.positive_number],											res.success_fail, None],
+	"accept_secret_chat": 	["accept_secret_chat", 	[args.secret_chat],												res.success_fail, None],
+	"send_contact": 		["send_contact", 		[args.peer, args.unicode_string, args.unicode_string, args.unicode_string], res.something, 	60.0], #ret: formated message
+	"status_online": 		["status_online", 		[],																res.success_fail, None],
+	"status_offline": 		["status_offline", 		[],																res.success_fail, None],
+	"quit": 				["quit", 				[],																res.success_fail, None],
+	"safe_quit": 			["safe_quit",	 		[],																res.success_fail, None],
+	"raw": 					["", 					[args.unescaped_unicode_string],								res.something, None]
 } 	# \{"(.*)",\ .*,\ \{\ (.*)\ \}\}, >> "$1": ["$1", [$2]],
 
 
-class DelayedFunctions():
-		def __init__(self, function_name, *arguments):
-			self.function_name = function_name
-			self.args = args
 _ANSWER_SYNTAX = b("ANSWER ")
 _LINE_BREAK = b("\n")
+
 class Sender(object):
+	default_answer_timeout = 0.2 # how long it should wait for a answer. DANGER: if set to None it will block!
 	def __init__(self, host, port):
+		"""
+
+		:param host:
+		:param port:
+		:param answer_timeout: how long it waits for the cli to answer, a float in seconds. Note that that commands like send_msg will not return anything, so a timeout makes sense to continue anyway.
+		:return:
+		"""
 		self.host = host
 		self.port_out = port
 		self.debug = False
@@ -82,24 +91,53 @@ class Sender(object):
 
 
 	def execute_function(self, function_name, *arguments):
+		"""
+		Execute a function.
+		:param function_name:
+		:param arguments:
+		:param answer_timeout:  set to a float to set a custom timeout for this answer.
+		:return:
+		"""
+
+		command_name, new_args = self._validate_input(function_name, arguments)
+		result_parser = functions[function_name][FUNC_RES]
+		result_timeout = functions[function_name][FUNC_TIME]
+		if result_timeout:
+			result = self._do_command(command_name, *new_args, answer_timeout = result_timeout)
+		else:
+			result = self._do_command(command_name, *new_args, answer_timeout = self.default_answer_timeout)
+
+		return result_parser(result)
+
+	@staticmethod
+	def _validate_input(function_name, arguments):
+		"""
+		:rtype : (srt, list)
+		"""
+		if not function_name in functions:
+			raise UnknownFunction(function_name)
+		command_name    = functions[function_name][FUNC_CMD]
 		arguments_types = functions[function_name][FUNC_ARGS]
-		command_name = functions[function_name][FUNC_CMD]
-		if (len(arguments) != len(arguments_types)):
-			raise ValueError("Error in function {function_name}: {expected_number} paramters expected, but {given_number} were given.".format(function_name=function_name, expected_number=len(arguments_types), given_number=len(args)))
+		if len(arguments) != len(arguments_types):
+			raise ValueError(
+				"Error in function {function_name}: {expected_number} paramters expected, but {given_number} were given.".format(
+					function_name=function_name, expected_number=len(arguments_types), given_number=len(arguments))
+			)
+		#end if
 		i = 0
 		new_args = []
 		for arg in arguments:
 			func_type = arguments_types[i]
 			# arg is the given one, which should be func_type.
 			if not func_type(arg):
-				raise ValueError("Error in function {function_name}: parameter {number} is not type {type}.".format(function_name=function_name, number=i, type=func_type.__name__))
+				raise ValueError("Error in function {function_name}: parameter {number} is not type {type}.".format(
+					function_name=function_name, number=i, type=func_type.__name__))
 			if func_type == args.unicode_string:
 				new_args.append(u(escape(arg)))
 			else:
 				new_args.append(u(str(arg)))
 			i += 1
-		# end for
-		return self._do_command(command_name, *new_args)
+		return command_name, new_args
 
 	def __getattr__(self, attr):
 		if attr in functions:
@@ -110,11 +148,11 @@ class Sender(object):
 		else:
 			return object.__getattribute__(self, attr)
 
-	def _do_command(self, function_sting, *argmts):
+	def _do_command(self, function_sting, *argmts, **kwargs):
 		arg_string = " ".join([str(x) for x in argmts])
 		request = " ".join([function_sting,  arg_string])
 		request = "".join([request, "\n"]) #TODO can this be deleted?
-		result = self._do_send(request)
+		result = self._do_send(request, **kwargs)
 		return result
 
 	class Command:
@@ -125,7 +163,7 @@ class Sender(object):
 		def __call__(self, *args):
 			return self.sender_instance.execute_function(self.name, *args)
 
-	def _do_send(self, command):
+	def _do_send(self, command, answer_timeout=default_answer_timeout):
 		if not isinstance(command, (text_type, binary_type)):
 			raise TypeError("Command to send is not a unicode(?) string. (Instead of %s you used %s.) " % (str(text_type), str(type(command))))
 		if self.debug:
@@ -158,10 +196,12 @@ class Sender(object):
 					print("Sended.")
 				completed = -1 # -1 = answer size yet unknown, >0 = got remaining answer size
 				buffer = b("")
+				s.settimeout(answer_timeout) # in seconds.
 				while completed != 0:
 					try:
-						s.settimeout(10) # in seconds.
 						answer = s.recv(1)
+						s.settimeout( max(self.default_answer_timeout, answer_timeout) ) # in seconds.
+						# If there was input the input is now either the default one or the given one, which waits longer.
 						buffer += answer
 						if completed < -1 and buffer[:len(_ANSWER_SYNTAX)] != _ANSWER_SYNTAX[:len(buffer)]:
 							raise ArithmeticError("Server response does not fit.")
