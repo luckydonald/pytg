ac8aa038ef7903127fd6ebd4b511e4ad4e4a0ef1
luckydonald <luckydonald@flutterb.at>
2015-05-11 14:44:08 +0200

    Fixed None returns.

--- pytg2/result_parser.py
+++ pytg2/result_parser.py
@@ -16,6 +16,7 @@ def nothing(value):
 def something(value):
 	if not (value and len(value) > 1):
 		raise IllegalResponseException("Should return something.")
+	return value
 
 def anything(value):
 	return value
