ce1edd866ed73fba757d4d291cf987b8dcbc1051
luckydonald <luckydonald@flutterb.at>
2016-03-16 19:22:41 +0100

    Simpler example printing the list of chats you have.

--- examples/command_dialog_list.py
+++ examples/command_dialog_list.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+Simpler example printing the list of chats you have.
+"""
+from pytg.sender import Sender
 __author__ = 'luckydonald'
 
 
 def main():
-    from pytg.sender import Sender
     x = Sender("127.0.0.1", 4458)
-    res = x.dialog_list()
-    print("Got: >%s<" % res)
+    result = x.dialog_list()
+    print("Got: %s" % str(result))
 
 if __name__ == '__main__':
     main()
